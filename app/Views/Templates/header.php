c513a793dd5f30cfa4ee1b9bacd4f8d3c10777f1
Bagas Adi Prayitno <aprayitno084@gmail.com>
2022-09-21 21:23:52 +0700

    Update

--- app/Views/Templates/header.php
+++ app/Views/Templates/header.php
@@ -37,7 +37,7 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <!-- <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navbar-right-top">
                         <li class="nav-item dropdown nav-user">
                             <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>
@@ -45,6 +45,6 @@
                             </div>
                         </li>
                     </ul>
-                </div>
+                </div> -->
             </nav>
         </div>
