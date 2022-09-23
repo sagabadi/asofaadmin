e593a439a6e51d12eefe1aaf9b98d2ca21567df7
Bagas Adi Prayitno <aprayitno084@gmail.com>
2022-09-23 10:26:42 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://hellobestiebyasofa.id/';
+	public $baseURL = 'https://somin.hellobestiebyasofa.id/';
 	// public $baseURL = 'https://intrasetneg.id/';
 
 	/**
